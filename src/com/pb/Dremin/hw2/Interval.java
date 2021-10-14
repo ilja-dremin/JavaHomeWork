0367663588e913bfe686687da6cc428453729bac
Ilja Dremin <ilja.dremin@privatbank.ua>
2021-10-14 23:07:18 +0300

    Interval

--- src/com/pb/Dremin/hw2/Interval.java
+++ src/com/pb/Dremin/hw2/Interval.java
@@ -6,17 +6,14 @@ public class Interval {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int x;
-        int y;
-        String sign;
 
-        System.out.println("введите значение x=");
+        System.out.println("введите значение числа от 1 до 100 и программа определит в какой из промежутков [0 -14] [15 - 35] [36 - 50] [51 - 100] оно входит");
         x = scan.nextInt();
-        System.out.println("Какую операцию хотите выполнить (доступны - сумирование, отнимание, умножение, деление) - введите символ операции");
-        sign = scan.next();
-        System.out.println("введите значение y=");
-        y = scan.nextInt();
 
-
-
+        if (x>=0 && x<=14) {System.out.println("Ваше значение " + x + " относится к промежутку [0 -14]" );}
+            else if (x>=15 && x<=35) {System.out.println("Ваше значение " + x + " относится к промежутку [15 - 35]" );}
+                else if (x>=36 && x<=50) {System.out.println("Ваше значение " + x + " относится к промежутку [36 - 50]" );}
+                    else if (x>=51 && x<=100) {System.out.println("Ваше значение " + x + " относится к промежутку [51 - 100]" );}
+                        else {System.out.println("Ваше значение " + x + " находится за пределами промежутка [0 - 100]" );}
     }
 }
